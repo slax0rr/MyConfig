8570f46e69fd15d67fa060fa4597edb177cbe8b9
Tomaz Lovrec <tomaz.lovrec@gmail.com>
2018-01-19 12:31:54 +0100

    update screenshot stuff

--- .bin/fullscreenshot.sh
+++ .bin/fullscreenshot.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 sleep 0.1
 img=$(date '+/tmp/%N.png')
-if [[ "$2" -eq "full" ]]; then
-    scrot -z $img >/dev/null 2>&1 || exit
-else
-    scrot -s -z $img >/dev/null 2>&1 || exit
-fi
+scrot -z $img >/dev/null 2>&1 || exit
 res=$(curl -F c=@$img https://ptpb.pw | awk -F'url:' '{print $2}') && (printf $res | xclip -f -selection c; printf "\a")
 notify-send `echo $res`

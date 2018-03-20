69bdab1095c9c6a8c0a8572d13d04a792a6c5f4d
Tomaz Lovrec <tomaz.lovrec@viberate.com>
2018-03-20 08:41:03 +0100

    add mouse switcher script and link it to a shortcut in i3

---
+++ .bin/laptop_mouse.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+XI=/usr/bin/xinput
+G=/usr/bin/grep
+C=/usr/bin/cut
+A=/usr/bin/awk
+
+TOUCHPADID=${TOUCHPADID:-13}
+TRACKPOINTID=${TRACKPOINTID:-14}
+
+TOUCHENABLED=`$XI list-props $TOUCHPADID | $G -i "device enabled" | $C -s -d: -f2 | $A {'print $1'}`
+TRACKENABLED=`$XI list-props $TRACKPOINTID | $G -i "device enabled" | $C -s -d: -f2 | $A {'print $1'}`
+
+TOUCHCMD="enable"
+if [[ $TOUCHENABLED -eq 1 ]]; then
+    TOUCHCMD="disable"
+fi
+TRACKCMD="enable"
+if [[ $TRACKENABLED -eq 1 ]]; then
+    TRACKCMD="disable"
+fi
+
+if [[ "$1" == "enable" ]] || [[ "$1" == "disable" ]]; then
+    TOUCHCMD=$1
+    TRACKCMD=$1
+fi
+
+$XI $TOUCHCMD "${TOUCHPADID}"
+$XI $TRACKCMD "${TRACKPOINTID}"

922c5cfa4d100f86297bd27f74a8acfa1527a7d6
Tomaz Lovrec <tomaz.lovrec@viberate.com>
2018-03-20 10:31:31 +0100

    update battery script

--- .bin/battery.sh
+++ .bin/battery.sh
@@ -8,19 +8,28 @@ WARNLVL=30
 DANGERLVL=10
 CRITLVL=5
 
+STATUS=`cat /sys/class/power_supply/BAT0/status`
+if [[ "$STATUS" -ne "Discharging" ]]; then
+    exit 0
+fi
 FULLENERGY=`cat /sys/class/power_supply/BAT0/energy_full`
 CURRENTENERGY=`cat /sys/class/power_supply/BAT0/energy_now`
 let CURRENTPERCENT=100*$CURRENTENERGY/$FULLENERGY
 
 if [[ $CURRENTPERCENT -le $CRITLVL ]]; then
-    $NS --urgency=critical --icon batery-empty "Battery level critical!" "Battery level is bellow $CRITLVL%, hibernating system in 5 seconds"
-    sleep 5
+    $NS --urgency=critical --icon batery-empty "Battery level critical!" "Battery level is bellow $CRITLVL%, hibernating system in 1 minute"
+    sleep 60
+    STATUS=`cat /sys/class/power_supply/BAT0/status`
+    if [[ "$STATUS" -eq "Discharging" ]]; then
+        $NS --urgency=critical --icon batery-low-charging "AC Connected" "Detected AC connection, aborting hibernation"
+        exit 0
+    fi
     sudo /usr/sbin/pm-hibernate
     exit 0
 elif [[ $CURRENTPERCENT -le $DANGERLVL ]]; then
-    $NS --urgency=critical --icon battery-critical "Battery level danger!" "Battery level is bellow $DANGERLVL%, plug in now!"
+    $NS --urgency=critical --icon battery-critical "Battery level danger!" "Battery level is bellow $DANGERLVL%, plug in now! ($CURRENTPERCENT%)"
     exit 0
 elif [[ $CURRENTPERCENT -le $WARNLVL ]]; then
-    $NS --icon battery "Battery level warning!" "Battery level is bellow $WARNLVL%"
+    $NS --icon battery "Battery level warning!" "Battery level is bellow $WARNLVL% ($CURRENTPERCENT%)"
     exit 0
 fi

764e00abe4db9f517582db90f5e98cfa4fa19973
Tomaz Lovrec <tomaz@lovrec.dev>
2024-12-13 18:40:14 +0100

    disable/enable autolock only on state change

--- .bin/stoplock.sh
+++ .bin/stoplock.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+DEBUG=0
+LOGFILE=/tmp/xautolock_debug.log
+STATEFILE="/tmp/xautolock_state.txt"
 MEDIA_PLAYER_TITLES=("YouTube" "Netflix" "Twitch" "Vimeo")
 NON_PLAYER_TITLES=("Meet -" "Meeting" "Zoom")
+
+log() {
+  if [ "$DEBUG" -eq 1 ]; then
+    echo "$(date) - $1" >> "$LOGFILE"
+  fi
+}
 
 is_matching_window() {
   local current_window="$1"
@@ -18,20 +27,57 @@ is_matching_window() {
 is_media_playing() {
   playerctl --player=brave status 2>/dev/null | grep -iq "playing"
 }
+if [ "$1" == "--debug" ]; then
+  DEBUG=1
+  echo "Debugging enabled"
+fi
+
+read_state() {
+  if [ -f "$STATEFILE" ]; then
+    cat "$STATEFILE"
+  else
+    echo "enabled"
+  fi
+}
+
+save_state() {
+  echo "$1" > "$STATEFILE"
+}
 
 while true; do
   current_window=$(i3-msg -t get_tree | jq -r '.. | objects | select(.focused==true) | .name')
+  current_state=$(read_state)
 
   if is_matching_window "$current_window" "${MEDIA_PLAYER_TITLES[@]}"; then
+    log "Player whitelisted window detected."
     if is_media_playing; then
-      xautolock -disable
+      log "Media playing detected."
+      if [ "$current_state" != "disabled" ]; then
+        log "Disabling xautolock."
+        xautolock -disable
+        save_state "disabled"
+      fi
     else
-      xautolock -enable
+      log "No media playing."
+      if [ "$current_state" != "enabled" ]; then
+        log "Enabling xautolock."
+        xautolock -enable
+        save_state "enabled"
+      fi
     fi
   elif is_matching_window "$current_window" "${NON_PLAYER_TITLES[@]}"; then
-    xautolock -disable
+    log "Non-player whitelisted window detected."
+    if [ "$current_state" != "disabled" ]; then
+      log "Disabling xautolock."
+      xautolock -disable
+      save_state "disabled"
+    fi
   else
-    xautolock -enable
+    if [ "$current_state" != "enabled" ]; then
+      log "Enabling xautolock."
+      xautolock -enable
+      save_state "enabled"
+    fi
   fi
 
   sleep 10

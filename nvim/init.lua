0b920baaef061c0088e91daf1688f6ebce0d2dd4
Tomaz Lovrec <tomaz@lovrec.dev>
2025-05-18 20:55:41 +0200

    add markdown preview to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -194,7 +194,15 @@ require("lazy").setup({
       require("Comment").setup()
     end,
     event = { "BufReadPost", "BufNewFile" },
-  }
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_auto_start = 0
+    end,
+  },
 })
 
 --------------------------

33b71c006eed858fc54ed07a115809714c672ded
Tomaz Lovrec <tomaz@lovrec.dev>
2025-05-29 17:10:20 +0200

    add the go configuration in nvim

---
+++ nvim/lua/config/go.lua
@@ -0,0 +1,46 @@
+local go = require("go")
+
+local function format_on_save(client, bufnr)
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format({ async = false })
+      vim.lsp.buf.code_action({
+        context = { only = { "source.organizeImports" } },
+        apply = true,
+      })
+    end,
+  })
+end
+
+go.setup({
+  lsp_inlay_hints = {
+    enable = true,
+  },
+  lsp_cfg = {
+    on_attach = format_on_save,
+    settings = {
+      gopls = {
+        hints = {
+          assignVariableTypes = true,
+          compositeLiteralFields = true,
+          compositeLiteralTypes = true,
+          constantValues = true,
+          functionTypeParameters = true,
+          parameterNames = true,
+          rangeVariableTypes = true,
+        },
+      },
+    },
+  },
+})
+
+vim.api.nvim_create_user_command("GoInlayForceToggle", function()
+  local current = vim.lsp.inlay_hint.is_enabled()
+
+  if current then
+    vim.lsp.inlay_hint.enable(false)
+  else
+    vim.lsp.inlay_hint.enable(true)
+  end
+end, {})
